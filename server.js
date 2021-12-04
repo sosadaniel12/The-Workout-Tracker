c2618b34bbb2e41bcc76b513a4bacc452e357a41
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-03 22:06:47 -0600

    fixed routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const api = require("./routes/api-routes");
+const html = require("./routes/html-routes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -18,8 +20,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(api);
+app.use(html);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
